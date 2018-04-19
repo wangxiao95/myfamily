eb6a320fbcb7a05f8257584ea73e4520b7a1c5d0
wangxiao <15133676286@163.com>
2018-04-19 19:42:07 +0800

    添加亲属组件     svg改为分组相对坐标

---
+++ src/components/AddModal.vue
@@ -0,0 +1,95 @@
+<template>
+  <!--添加模态框-->
+  <Modal
+    v-model="modal.flag"
+    title="添加亲属"
+    @on-ok="ok"
+    @on-cancel="cancel">
+    <Form :model="formItem" :label-width="80">
+      <FormItem label="姓名">
+        <Input v-model="formItem.name" placeholder="请输入姓名"></Input>
+      </FormItem>
+      <FormItem label="关系">
+        <Select v-model="formItem.ship">
+          <Option value="father">父亲</Option>
+          <Option value="mother">母亲</Option>
+          <Option value="sister">姐妹</Option>
+          <Option value="brother">兄弟</Option>
+          <Option value="children">子女</Option>
+        </Select>
+      </FormItem>
+      <FormItem label="生日">
+        <Row>
+          <Col span="11">
+            <DatePicker type="date" placeholder="Select date" v-model="formItem.birthday"></DatePicker>
+          </Col>
+          <!--<Col span="2" style="text-align: center">-</Col>-->
+          <!--<Col span="11">-->
+          <!--<TimePicker type="time" placeholder="Select time" v-model="formItem.time"></TimePicker>-->
+          <!--</Col>-->
+        </Row>
+      </FormItem>
+      <FormItem label="性别">
+        <RadioGroup v-model="formItem.sex">
+          <Radio label="m">Male</Radio>
+          <Radio label="f">Female</Radio>
+        </RadioGroup>
+      </FormItem>
+      <!--<FormItem label="Checkbox">-->
+      <!--<CheckboxGroup v-model="formItem.checkbox">-->
+      <!--<Checkbox label="Eat"></Checkbox>-->
+      <!--<Checkbox label="Sleep"></Checkbox>-->
+      <!--<Checkbox label="Run"></Checkbox>-->
+      <!--<Checkbox label="Movie"></Checkbox>-->
+      <!--</CheckboxGroup>-->
+      <!--</FormItem>-->
+      <!--<FormItem label="Switch">-->
+      <!--<i-switch v-model="formItem.switch" size="large">-->
+      <!--<span slot="open">On</span>-->
+      <!--<span slot="close">Off</span>-->
+      <!--</i-switch>-->
+      <!--</FormItem>-->
+      <!--<FormItem label="Slider">-->
+      <!--<Slider v-model="formItem.slider" range></Slider>-->
+      <!--</FormItem>-->
+      <FormItem label="个人简介">
+        <Input v-model="formItem.textarea" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="Enter something..."></Input>
+      </FormItem>
+      <!--<FormItem>-->
+      <!--<Button type="primary">Submit</Button>-->
+      <!--<Button type="ghost" style="margin-left: 8px">Cancel</Button>-->
+      <!--</FormItem>-->
+    </Form>
+  </Modal>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        //弹窗表单
+        formItem: {
+          name: '',
+          ship: '',
+          sex: '',
+          checkbox: [],
+          switch: true,
+          date: '',
+          time: '',
+          slider: [20, 50],
+          textarea: ''
+        },
+      }
+    },
+    props: ['modal'],
+    methods: {
+      ok () {
+        // this.self.relationship[this.formItem.ship].push(this.formItem);
+        this.$emit('addPersonal', this.formItem)
+      },
+      cancel () {
+
+      }
+    }
+  }
+</script>
--- src/components/SvgCom.vue
+++ src/components/SvgCom.vue
@@ -1,50 +1,47 @@
 <template>
   <div class="box" id="box">
-    <svg>
+    <svg preserveAspectRatio="xMinYMin meet" viewBox="300,300,1000,800" @mousemove="move" @mousedown="getPoint">
       <g v-for="(val, key) in self.relationship">
-        <g v-if="!Array.isArray(val)">
-          <rect :x="computedX(key)" :y="computedY(key)" rx="5" ry="5" width="150" height="60" :fill="bgColor[val.sex]" @click="test"
+        <g v-if="!Array.isArray(val)" :transform="getTransform(key)">
+          <rect x="0" y="0" rx="5" ry="5" width="150" height="60" :fill="bgColor[val.sex]" @click="test"
                 style="stroke:black;stroke-width:0;"/>
-          <image :x="computedX(key)+10" :y="computedY(key)+10" xlink:href="../assets/img/3.jpg" width="40" height="40"/>
-          <text :x="computedX(key)+70" :y="computedY(key)+25" fill="black">{{val.name}}</text>
-          <text :x="computedX(key)+130" :y="computedY(key)+25" fill="black" @click="test">{{sex[val.sex]}}</text>
-          <text :x="computedX(key)+70" :y="computedY(key)+45" fill="black" @click="test">{{val.ship}}</text>
-          <text :x="computedX(key)+130" :y="computedY(key)+45" fill="black" @click="test">50</text>
-          <circle :cx="computedX(key)+75" :cy="computedY(key)+60" r="10" stroke="black" stroke-width="0" fill="#fff"/>
-          <circle :cx="computedX(key)+75" :cy="computedY(key)+60" r="8" stroke="black" stroke-width="0" fill="#5cadff" @click="test"/>
-          <line :x1="computedX(key)+70" :y1="computedY(key)+60" :x2="computedX(key)+80" :y2="computedY(key)+60" style="stroke:rgb(255,255,255);stroke-width:2" @click="test"/>
-          <line :x1="computedX(key)+75" :y1="computedY(key)+55" :x2="computedX(key)+75" :y2="computedY(key)+65" style="stroke:rgb(255,255,255);stroke-width:2" @click="test"/>
+          <image x="10" y="10" xlink:href="../assets/img/3.jpg" width="40" height="40"/>
+          <text x="70" y="25" fill="black">{{val.name}}</text>
+          <text x="130" y="25" fill="black" @click="test">{{sex[val.sex]}}</text>
+          <text x="70" y="45" fill="black" @click="test">{{key}}</text>
+          <text x="130" y="45" fill="black" @click="test">50</text>
+          <circle cx="75" cy="60" r="10" stroke="black" stroke-width="0" fill="#fff"/>
+          <circle cx="75" cy="60" r="8" stroke="black" stroke-width="0" fill="#5cadff" @click="showModal"/>
+          <line x1="70" y1="60" x2="80" y2="60" style="stroke:rgb(255,255,255);stroke-width:2" @click="showModal"/>
+          <line x1="75" y1="55" x2="75" y2="65" style="stroke:rgb(255,255,255);stroke-width:2" @click="showModal"/>
         </g>
-        <g v-else v-for="(item, i) in val">
-          <rect :x="computedX(key, i)" :y="computedY(key, i)" rx="5" ry="5" width="150" height="60" :fill="bgColor[item.sex]" @click="test"
+        <g v-else v-for="(item, i) in val" :transform="getTransform(key, i)">
+          <rect x="0" y="0" rx="5" ry="5" width="150" height="60" :fill="bgColor[item.sex]" @click="test"
                 style="stroke:black;stroke-width:0;"/>
-          <image :x="computedX(key, i)+10" :y="computedY(key, i)+10" xlink:href="../assets/img/3.jpg" width="40" height="40"/>
-          <text :x="computedX(key, i)+70" :y="computedY(key, i)+25" fill="black">{{item.name}}</text>
-          <text :x="computedX(key, i)+130" :y="computedY(key, i)+25" fill="black" @click="test">{{sex[item.sex]}}</text>
-          <text :x="computedX(key, i)+70" :y="computedY(key, i)+45" fill="black" @click="test">{{item.ship}}</text>
-          <text :x="computedX(key, i)+130" :y="computedY(key, i)+45" fill="black" @click="test">50</text>
-          <circle :cx="computedX(key, i)+75" :cy="computedY(key, i)+60" r="10" stroke="black" stroke-width="0" fill="#fff"/>
-          <circle :cx="computedX(key, i)+75" :cy="computedY(key, i)+60" r="8" stroke="black" stroke-width="0" fill="#5cadff" @click="test"/>
-          <line :x1="computedX(key, i)+70" :y1="computedY(key, i)+60" :x2="computedX(key, i)+80" :y2="computedY(key, i)+60" style="stroke:rgb(255,255,255);stroke-width:2" @click="test"/>
-          <line :x1="computedX(key, i)+75" :y1="computedY(key, i)+55" :x2="computedX(key, i)+75" :y2="computedY(key, i)+65" style="stroke:rgb(255,255,255);stroke-width:2" @click="test"/>
+          <image x="10" y="10" xlink:href="../assets/img/3.jpg" width="40" height="40"/>
+          <text x="70" y="25" fill="black">{{item.name}}</text>
+          <text x="130" y="25" fill="black" @click="test">{{sex[item.sex]}}</text>
+          <text x="70" y="45" fill="black" @click="test">{{key}}</text>
+          <text x="130" y="45" fill="black" @click="test">50</text>
+          <circle cx="75" cy="60" r="10" stroke="black" stroke-width="0" fill="#fff"/>
+          <circle cx="75" cy="60" r="8" stroke="black" stroke-width="0" fill="#5cadff" @click="showModal(item.name)"/>
+          <line x1="70" y1="60" x2="80" y2="60" style="stroke:rgb(255,255,255);stroke-width:2" @click="showModal(item.name)"/>
+          <line x1="75" y1="55" x2="75" y2="65" style="stroke:rgb(255,255,255);stroke-width:2" @click="showModal(item.name)"/>
         </g>
       </g>
     </svg>
-    <button class="pl20" @click="test">test</button>
-    <!--<ul>-->
-      <!--<li v-for="(v, k) in self.relationship">-->
-        <!--{{v}}-->
-        <!--&lt;!&ndash;<span v-if="!Array.isArray(v)">{{v}}</span>&ndash;&gt;-->
-        <!--&lt;!&ndash;<span v-else v-for="item in v">{{item}}</span>&ndash;&gt;-->
-      <!--</li>-->
-    <!--</ul>-->
+    <!--添加亲属modal-->
+    <Add-modal :modal="modal" @addPersonal="addPersonal"></Add-modal>
   </div>
 </template>
 
 <script>
+import AddModal from '../components/AddModal'
 export default {
   data () {
     return {
+      svgX: 0,
+      svgY: 0,
       //以自己为中心的初始坐标
       x: 400,
       y: 300,
@@ -58,18 +55,22 @@ export default {
         m: '♀',
         f: '♂'
       },
+      //控制模态框
+      modal: {
+        flag: false,
+        title: ''
+      },
+
       //数据
       self: {
         name: '本人',
         relationship: {
           self: {
             name: '我',
-            ship: '自己',
             sex: 'f'
           },
           father: {
             name: '父亲',
-            ship: '父亲',
             sex: 'f'
           },
           mother: {
@@ -89,32 +90,21 @@ export default {
             {name: '兄弟1', ship: '兄弟', sex: 'f'},
             {name: '兄弟2', ship: '兄弟', sex: 'f'},
           ],
-          // son: [
-          //   {name: '儿子1'},
-          //   {name: '儿子2'}
-          // ],
-          // daughter: [
-          //   {name: '女儿1'},
-          //   {name: '女儿2'}
-          // ]
+          children: [
+            {name: '儿子', ship: '儿子', sex: 'f'},
+            {name: '女儿', ship: '女儿', sex: 'm'}
+          ]
         }
       }
     }
   },
   computed: {
-    fatherX () { return this.x - 50 },
-    fatherY () { return this.y + 100 },
-    motherX () {
 
-    },
-    motherY () {
-
-    }
   },
   methods: {
-    test () {
-
-      alert(document.getElementById('box').clientHeight)
+    test (a) {
+      console.log(a)
+      alert(a)
 
     },
     //计算每个item的x坐标
@@ -133,6 +123,8 @@ export default {
           return this.x - 200 * i;
         case 'sister':
           return this.x + this.self.relationship.partner.length * 200 + 200 * i;
+        case 'children':
+          return this.x + 200 * (i - 1);
       }
     },
     //计算每个item的y坐标
@@ -151,14 +143,36 @@ export default {
           return this.y;
         case 'sister':
           return this.y;
+        case 'children':
+          return this.y + 130;
       }
+    },
+    getTransform (key, i) {
+      return `translate(${this.computedX(key, i)}, ${this.computedY(key, i)})`;
+    },
+    showModal () {
+      this.modal.flag = true;
+    },
+    addPersonal (data) {
+      console.log(data);
+      this.self.relationship[data.ship].push(data);
+    },
+    move (e) {
+      this.svgX += 10
+      this.svgY += 10
+    },
+    getPoint (e) {
+      console.log(e.offsetX)
+      console.log(e.offsetY)
     }
-
   },
   watch: {
     t () {
       alert('#ff66cd,#535fbe')
     }
+  },
+  components: {
+    AddModal
   }
 }
 </script>
@@ -174,5 +188,8 @@ export default {
       width: 100%;
       height: 100%;
     }
+  }
+  svg{
+
   }
 </style>
